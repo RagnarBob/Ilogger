11d46fa9a104803b041e9b5d257f3fee21b64172
Louis Midson LAJEANTY <louismidsonlajeanty@gmail.com>
2023-11-13 17:25:09 -0500

    Fix Error, Improve code and update tests

--- iloger/logger.py
+++ iloger/logger.py
@@ -1,13 +1,28 @@
-def write_log(filename, content):
-    with open(filename, 'a') as f:
+import os
+
+base_path = 'logs'
+
+def create_dir_if_not_exist(path:str) -> None:
+    if not os.path.exists(path):
+        os.makedirs(path)
+
+def write_log(filename:str, content:str, reset:bool) -> None:
+    mode = 'w' if reset else 'a'
+
+    create_dir_if_not_exist(base_path)
+
+    with open(f"{base_path}/{filename}", mode) as f:
         f.write(content)
 
-def read_logs(filename):
+def read_logs(filename:str) -> list[str]:
     try:
-        with open(filename) as f:
+        with open(f"{base_path}/{filename}") as f:
             return f.readlines()
     except FileNotFoundError:
-        f = open(filename, 'w')
+
+        create_dir_if_not_exist(base_path)
+
+        f = open(f"{base_path}/{filename}", 'w')
         f.close()
         return []
         
